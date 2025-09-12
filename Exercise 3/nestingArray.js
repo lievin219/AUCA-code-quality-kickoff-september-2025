e7154a12830fcabb5d01358acdde2c495a6fa854
Gym Bigogwe <gymbigogwe@Bigogwes-iMac.local>
2025-09-12 14:47:50 +0200

    added some changes

---
+++ Exercise 3/nestingArray.js
@@ -0,0 +1,14 @@
+// Write a function(s) that checks the level of nesting of an array. Then, use that function to flatten the array into a single-dimensional array.
+
+ const nestedArray=[1,2,[3,4],[5]]
+
+  function getNestedArray(newArray){
+     if(!Array.isArray(newArray)){
+        return 0
+     }
+     const depth=1;
+       for ( const item  of newArray ){
+
+        
+       }
+  }
