76424d8fc000eff04fb0e92307c021f900e4371a
Gym Bigogwe <gymbigogwe@Bigogwes-iMac.local>
2025-09-12 15:19:45 +0200

    feat:REMOVING DUPLICATES

---
+++ Exercise 3/removeDuplicate.js
@@ -0,0 +1,15 @@
+function removeDuplicate(array){
+     let result=[]
+
+     if (!Array.isArray(array)){
+
+         return 'this is not an array !'
+     }
+     for (const item of array){
+        if(!result.includes(item)){
+            result.push(item)
+        }
+    }
+      return result
+     
+}
