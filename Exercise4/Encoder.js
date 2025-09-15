6262bdef5d34a85c2f911630e724a0d5e946ddd6
Gym Bigogwe <gymbigogwe@Bigogwes-iMac.local>
2025-09-15 15:31:19 +0200

    feat:Encoder

--- Exercise4/Encoder.js
+++ Exercise4/Encoder.js
@@ -1,3 +1,27 @@
-function Encoder(){
-      
-}
+function Encoder(num) {
+   
+    const symbols = [
+      "M",  "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
+    ];
+    const values = [
+      1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
+    ];
+  
+    let result = "";
+    let i = 0;
+  
+    while (num > 0) {
+      if (num >= values[i]) {
+        result += symbols[i];
+        num -= values[i];
+      } else {
+        i++; 
+      }
+    }
+  
+    return result;
+  }
+
+   console.log(Encoder(20))
+  
+ 
